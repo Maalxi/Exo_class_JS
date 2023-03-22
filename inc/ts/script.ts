c85460462c775db7e567d181d0693d7a389f2ea8
Maalxi <maalxi38@gmail.com>
2023-03-22 10:44:10 +0100

    Mise à jour le 22-03-2023 - fin exercice

--- inc/ts/script.ts
+++ inc/ts/script.ts
@@ -1,1 +1,57 @@
+class Pokemon {
+  // Propriétés
+  health: number = 100
+  
+  // Méthodes
+  pokemon_health() {
+    console.log(`Votre Pokemon a : ${this.health} point de vie`)
+  }
+}
 
+class carapuce extends Pokemon {
+  // Propriétés
+  info: string = 'Carapuce est de type eau'
+
+  // Méthodes
+  carapuce_info() {
+    console.log(this.info);
+  }
+}
+
+class bulbizar extends Pokemon {
+  // Propriétés
+  info: string = 'Bulbizar est de type plante'
+
+  // Méthodes
+  bulbizar_info() {
+    console.log(this.info);
+  }
+}
+
+class salameche extends Pokemon {
+  // Propriétés
+  info: string = 'Salameche est de type feu'
+
+  // Méthodes
+  salameche_info() {
+    console.log(this.info);
+  }
+}
+
+// J'instancie un objet avec la class Carapuce
+const Carapuce = new carapuce
+
+Carapuce.carapuce_info()
+Carapuce.pokemon_health()
+
+// J'instancie un objet avec la class Carapuce
+const Bulbizar = new bulbizar
+
+Bulbizar.bulbizar_info()
+Bulbizar.pokemon_health()
+
+// J'instancie un objet avec la class Carapuce
+const Salameche = new salameche
+
+Salameche.salameche_info()
+Salameche.pokemon_health()
